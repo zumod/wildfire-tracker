384f3e5a9077676c3b6230f35859524314890d46
Sumod Sivadas <contactsumod@gmail.com>
2020-11-24 02:10:46 +0530

    Added Header component

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -1,7 +1,7 @@
 const LocationInfoBox = ({ info }) => {
     return (
         <div className="location-info">
-            <h2>Event Location Info</h2>
+            <h3>Event Location Info</h3>
             <ul>
     <li>ID: <strong>{ info.id }</strong></li>
     <li>TITLE: <strong>{ info.title }</strong></li>
