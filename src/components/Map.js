8746e2ce865c5aec194716168c6862d830abaf2f
Sumod Sivadas <contactsumod@gmail.com>
2020-11-24 00:12:51 +0530

    Added location marker component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,15 @@
 import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
 
 const Map = ({ center, zoom }) => {
     return (
         <div className="map">
             <GoogleMapReact
-                bootstrapURLKeys={{ key: 'AIzaSyBikq6ORYkzZh9n_EsGl8INqx-7XhqI_fo' }}
+                bootstrapURLKeys={{ key: '' }}
                 defaultCenter={ center }
                 defaultZoom={ zoom }
             >
-
+                <LocationMarker lat={center.lat} lng={center.lng} />
             </GoogleMapReact>
         </div>
     )
